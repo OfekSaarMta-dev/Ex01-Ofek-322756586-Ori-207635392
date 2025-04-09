0f072324c403daeaa163c6dfc471ce7dd06a9877
אופק סער <ofeksa@mta.ac.il>
2025-04-09 10:11:55 +0300

    Add project files.

---
+++ Ex01_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_01
+{
+    class Program
+    {
+        static void Main()
+        {
+          Console.WriteLine();
+        }
+    }
+}
